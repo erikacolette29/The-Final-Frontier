50aa8334267ec62a3618a0a30a08960df63a0f21
erikacolette <erikatidwell.c@gmail.com>
2020-11-16 17:18:39 -0500

    js start

--- js/app.js
+++ js/app.js
@@ -3,17 +3,29 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-
+const planetData = []
+const shipData = []
 
 
 /*------------------------ Cached Element References ------------------------*/
 
+const planets = document.querySelectorAll('.planets')
+const ship = document.getElementById('enterprise')
+const soundBtn = document.getElementById('sound-btn')
+const darkBtn = document.getElementById('dark-btn')
 
-
+const container = document.getElementById('containerDiv')
+const body = document.getElementById('body')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+planets.addEventListener('click', () => {})
 
+ship.addEventListener('click', () =>{})
+
+soundBtn.addEventListener('click', ())
+
+darkBtn.addEventListener('click', ())
 
 
 /*-------------------------------- Functions --------------------------------*/
