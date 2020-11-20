9502813fae37ee469abd221b2afaa961fd584774
erikacolette <erikatidwell.c@gmail.com>
2020-11-20 18:48:32 -0500

    organization of files

--- js/app.js
+++ js/app.js
@@ -1,60 +1,45 @@
-/*-------------------------------- Constants 
---------------------------------*/
 const container = document.getElementById("containerDiv");
 const container2 = document.getElementById("containerDiv2");
 
+const planets = document.querySelectorAll(".planets");
+const ship = document.getElementById("enterprise");
+const borg = document.getElementById("borg");
+
+const soundBtn = document.getElementById("sound-btn");
+const darkBtn = document.getElementById("dark-btn");
+
+const body = document.getElementById("body");
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+document.querySelector(".planets-container").addEventListener("click", handleClick);
+
+soundBtn.addEventListener("click", personClick);
+
 function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
-  
-
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+/*-------------------------------- Functions --------------------------------*/
+// Remove Button //
 function myRemove() {
-   container.style.display = "none";
-  }
-  function myRemove2() {
-    container2.style.display = "none";
-   }
-  
-  
-  /*-------------------------------- Variables --------------------------------*/
-  
-  
-  /*------------------------ Cached Element References ------------------------*/
-  
-  const planets = document.querySelectorAll(".planets");
-  const ship = document.getElementById("enterprise");
-  const soundBtn = document.getElementById("sound-btn");
-  const darkBtn = document.getElementById("dark-btn");
-  
+  container.style.display = "none";
+}
+function myRemove2() {
+  container2.style.display = "none";
+}
 
-  const body = document.getElementById("body");
+//   PLANETS API//
+function handleClick(click) {
+  let planet = click.target.id;
+  console.log(planet);
+  const planetsUrl = `https://api.le-systeme-solaire.net/rest/bodies/${planet}`;
+  async function getPlanets() {
+    const response = await fetch(planetsUrl);
+    const data = await response.json();
+    const { englishName, gravity, moons, density, escape } = data;
 
-  const borg = document.getElementById('borg')
-
- 
-  
-  /*----------------------------- Event Listeners -----------------------------*/
-  
-  document.querySelector(".planets-container").addEventListener("click", handleClick);
-
-
-soundBtn.addEventListener('click', personClick)
-
-  
-  /*-------------------------------- Functions --------------------------------*/
-  
-//   PLANETS INFO //
-  function handleClick(click) {
-    let planet = click.target.id;
-    console.log(planet);
-    const planetsUrl = `https://api.le-systeme-solaire.net/rest/bodies/${planet}`;
-    async function getPlanets() {
-      const response = await fetch(planetsUrl);
-      const data = await response.json();
-      const { englishName, gravity, moons, density, escape } = data;
-
-      document.getElementById("containerDiv").innerHTML = ` Captain's log,
+    document.getElementById("containerDiv").innerHTML = ` Captain's log,
        We passed by ${englishName} today, 
       with gravity strength of ${gravity}.
      We reported ${moons ? moons.length : 0} moons, and density of ${density}. 
@@ -63,26 +48,25 @@ soundBtn.addEventListener('click', personClick)
    <button id='remove' onclick="myRemove()">Remove</button>
 
    `;
-    }
-   
-    container.style.display = "block"
-    getPlanets();
-  
   }
-  
-//ENTERPRISE //
 
-  ship.addEventListener('click', (e) =>{
-      let audioVar = new Audio(`/sound/enterprise.mp3`)
-      audioVar.play()
-  })
+  container.style.display = "block";
+  getPlanets();
+}
 
-//   BorgShip //
+//ENTERPRISE Sound//
 
-borg.addEventListener('click', (e) =>{
-    let audio = new Audio(`/sound/borg.wav`)
-    audio.play()
-})
+ship.addEventListener("click", (e) => {
+  let audioVar = new Audio(`/sound/enterprise.mp3`);
+  audioVar.play();
+});
+
+// Borg Sound //
+
+borg.addEventListener("click", (e) => {
+  let audio = new Audio(`/sound/borg.wav`);
+  audio.play();
+});
 
 // Person //
 function personClick(click) {
@@ -92,13 +76,15 @@ function personClick(click) {
   async function peopleSpace() {
     const response = await fetch(peopleUrl);
     const data = await response.json();
-    const {number, people} = data;
+    const { number, people } = data;
     let craft;
-    people.map((c)=>{
-      console.log(c)
-    craft = c.craft
-    })
-    document.getElementById("containerDiv2").innerHTML = ` Captain's Log, supplemental.
+    people.map((c) => {
+      console.log(c);
+      craft = c.craft;
+    });
+    document.getElementById(
+      "containerDiv2"
+    ).innerHTML = ` Captain's Log, supplemental.
      We were thrown into a wormhole
       and time traveled back to the year 2020.
        Commander Data reported that as of this moment,
@@ -108,10 +94,8 @@ function personClick(click) {
         
         <button id='remove' onclick="myRemove2()">Remove</button>
   `;
-}
+  }
 
-  container2.style.display = "block"
- peopleSpace();
- 
+  container2.style.display = "block";
+  peopleSpace();
 }
-
